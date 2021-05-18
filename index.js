8479ed4e77f0a7c2d3a73dc2296be32b84e3d79e
Nikolay <freshitftm@gmail.com>
2021-05-18 10:51:06 +0300

    Add File System Task Solution

--- index.js
+++ index.js
@@ -1,36 +1,15 @@
 "use strict";
-
-const MyMath = require("./MyMath");
-const {Component, test} = require("./Component");
-
-const componentObj = require("./test");
-
-console.log("Hello node!");
-console.log(2 + 2);
-
-console.log(MyMath.sum(1,2,3,4,5));
-console.log(Component);
-console.log(test);
+const fs = require("fs").promises;
+fs.readFile("./test.txt", "utf8")
+  .then((data) => fs.writeFile("./newFile.txt", `${data}\nNEW CONTENT`))
+  .then(() => {
+    console.log("success");
+  })
+  .catch((e) => {
+    console.error(e);
+  });
 
 /*
-How does require work?
-
-1) Resolving - поск файла
-2) Loading - загрузка файла
-3) Wrapping - добавление обетрки для модуля
-4) Evaluation - выполнение кода
-5) Caching - кеширование выполненного кода
-*/
-
-/*
- Require file search stages
-1) Common modules
-2) File
-  2.1) *.js
-  2.2) *.json
-3) Directory
-  3.1) package.json -> main
-  3.2) index.js | index.json
-4) node_modules
-5) throw new Error()
-*/
+  1) Прочесть файл
+  2) Создать новый файл, который скопирует содержимое первого файла + "NEW CONTENT" в конце с новой строки
+  */
