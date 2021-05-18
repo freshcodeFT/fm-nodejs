26f519e3664b8e999f088bb28066e61a6448a497
Nikolay <freshitftm@gmail.com>
2021-05-18 12:15:41 +0300

    Add webpack build example

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const lodash = require('lodash');
-console.log(lodash.random(0, 5));
-
-00001
-00012
-01235
-43423
---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const lodash = require('lodash');
+const lorem = require('./lorem');
+console.log("INDEX START");
+console.log(lorem);
+console.log(lodash.random(0, 5));
+console.log('INDEX END');
