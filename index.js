d44bcac7973fb2c96a86f74f400c598ec6eb4998
Nikolay <freshitftm@gmail.com>
2021-05-18 11:30:21 +0300

    Add lodash

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
 "use strict";
-const fs = require("fs").promises;
-fs.readFile("./test.txt", "utf8")
-  .then((data) => fs.writeFile("./newFile.txt", `${data}\nNEW CONTENT`))
-  .then(() => {
-    console.log("success");
-  })
-  .catch((e) => {
-    console.error(e);
-  });
-
-/*
-  1) Прочесть файл
-  2) Создать новый файл, который скопирует содержимое первого файла + "NEW CONTENT" в конце с новой строки
-  */
+const lodash = require('lodash');
+console.log(lodash.random(0, 5));
